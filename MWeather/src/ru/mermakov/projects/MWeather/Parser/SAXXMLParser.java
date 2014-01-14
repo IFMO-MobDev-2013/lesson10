20817beca8fe941d37caff0c82e1b21fd77ba93d
Mikhail Ermakov <mikhail@mermakov.ru>
2014-01-14 17:07:35 +0400

    1

---
+++ MWeather/src/ru/mermakov/projects/MWeather/Parser/SAXXMLParser.java
@@ -0,0 +1,36 @@
+package ru.mermakov.projects.MWeather.Parser;
+
+import android.util.Log;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import javax.xml.parsers.SAXParserFactory;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SAXXMLParser {
+
+    String xml;
+
+    public SAXXMLParser(String xml) {
+        this.xml = xml;
+    }
+
+    public ArrayList<HashMap<String, String>> parse() {
+        ArrayList<HashMap<String, String>> items = null;
+        try {
+            XMLReader xmlReader = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
+            ru.mermakov.projects.MWeather.Parser.SAXXMLHandler saxHandler = new SAXXMLHandler();
+            xmlReader.setContentHandler(saxHandler);
+            InputSource is = new InputSource();
+            is.setCharacterStream(new StringReader(xml));
+            xmlReader.parse(is);
+            items = saxHandler.getItems();
+
+        } catch (Exception ex) {
+            Log.d("SAXXMLParser", "parsing failed");
+        }
+        return items;
+    }
+}
