9dad81a6a78b0fcb5e5ae1dea066f5e48c306953
Gregory Fefelov <gfv@zoidberg.md>
2014-02-03 16:20:27 +0400

    initial empty project

---
+++ src/md/zoidberg/android/forecast/ForecastActivity.java
@@ -0,0 +1,15 @@
+package md.zoidberg.android.forecast;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ForecastActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
